58c294eafa7bd158a4bf65aa11556565b47a5116
NZEFER <zeferovich@mail.ru>
2025-07-17 19:51:14 +0300

    add migration and kafka config

--- src/main/java/ZEFER/PAYMENTSERVICE/Model/Entity/Refund.java
+++ src/main/java/ZEFER/PAYMENTSERVICE/Model/Entity/Refund.java
@@ -2,14 +2,20 @@ package ZEFER.PAYMENTSERVICE.Model.Entity;
 
 import ZEFER.PAYMENTSERVICE.Model.Enums.Convertor.RefundStatusConvertor;
 import ZEFER.PAYMENTSERVICE.Model.Enums.RefundStatus;
-import jakarta.persistence.Convert;
-import jakarta.persistence.Converter;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.hibernate.mapping.Join;
 
 import java.math.BigDecimal;
 
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Refund extends BaseEntity{
     private BigDecimal refundAmount;
     private String reason;
